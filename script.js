5e62784e7849931772efb4a41d754bebba55a8ea
EmelyV23 <emelyvb2018@gmail.com>
2024-03-14 16:56:59 -0400

    ALAB part 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// import "./styles.css";  not needed
+//Select and cache the <main> element in a variable named mainEl.
+let mainE1 = document.querySelector('main');
+console.log(mainE1);
+//Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+mainE1.style.backgroundColor = '#4a4e4d'
+
+//Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+// mainE1.textContent = 'DOM Manipulation'
+mainE1.innerHTML = 'DOM Manipulation'
+
+//Add a class of flex-ctr to mainEl.
+//Hint: Use the Element.classList API.
+mainE1.classList.add('flex-ctr');
