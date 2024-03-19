2ca14f60f5324542ae25e9e17df60e2757c363a2
EmelyV23 <emelyvb2018@gmail.com>
2024-03-18 23:52:48 -0400

    mostPartTwo

--- script.js
+++ script.js
@@ -14,10 +14,41 @@ mainE1.innerHTML = 'DOM Manipulation'
 mainE1.classList.add('flex-ctr');
 
 const topMenuEl = ("top-menu");
+const subMenuEl = ("sub-menu");
+const topMenuLinks = topMenuEl.querySelectorAll('a');
+
+topMenuEl.addEventListener('click', function(evt){
+if (evt.target === topMenuEl){
+    console.log('not a link')
+}else{
+    console.log(evt.target)
+}
+const clicked = evt.target;
+clicked.classList.toggle('active')
+
+topMenuLinks.forEach(link => {
+    if (link !== evt.target){
+        link.classList.remove('active')
+    }
+    });
+}
+)
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+    {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
   ];
+
